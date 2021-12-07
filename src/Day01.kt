1e5f48586523012fe19ce67562d229b7931588ee
Ian Weng <ian.weng@lightspeedhq.com>
2021-12-07 16:14:23 +1300

    day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,25 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        var result = 0
+        for (i in 1 until input.size)
+            if (input[i] > input[i - 1])
+                result++
+        return result
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        var result = 0
+        for (i in 3 until input.size)
+            if (input[i] > input[i - 3])
+                result++
+        return result
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_test").map { it.toInt() }
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01").map { it.toInt() }
     println(part1(input))
     println(part2(input))
 }
